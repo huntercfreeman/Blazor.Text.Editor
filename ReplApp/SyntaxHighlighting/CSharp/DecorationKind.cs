b7f81bcf5e208a89f7e9f00e16fd5c58f2fd9f35
Hunter Freeman <hunter.freeman@darkmatter2bd.com>
2022-10-12 00:03:58 -0400

    WebAssembly version

---
+++ ReplApp/SyntaxHighlighting/CSharp/DecorationKind.cs
@@ -0,0 +1,12 @@
+﻿namespace ReplApp.SyntaxHighlighting.CSharp;
+
+public enum DecorationKind
+{
+    None,
+    Method,
+    Type,
+    Parameter,
+    StringLiteral,
+    Keyword,
+    Comment
+}
---
+++ ReplApp/SyntaxHighlighting/FictitiousLanguage/TextEditorFictitiousLanguageLexer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Immutable;
+using BlazorTextEditor.RazorLib.Lexing;
+
+namespace ReplApp.SyntaxHighlighting.FictitiousLanguage;
+
+public class TextEditorFictitiousLanguageLexer : ILexer
+{
+    public Task<ImmutableArray<TextEditorTextSpan>> Lex(string content)
+    {
+        return Task.FromResult(ImmutableArray<TextEditorTextSpan>.Empty);
+    }
+}
