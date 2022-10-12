99cea860ac0092d81be7b1068d8747a224e6699c
Hunter Freeman <hunter.freeman@darkmatter2bd.com>
2022-10-12 14:30:52 -0400

    Move things over to Wasm app

---
+++ ReplApp/Themes/ThemeDisplay.razor.cs
@@ -0,0 +1,10 @@
+﻿using BlazorTextEditor.RazorLib.Store.ThemeCase;
+using Microsoft.AspNetCore.Components;
+
+namespace ReplApp.Themes;
+
+public partial class ThemeDisplay : ComponentBase
+{
+    [Parameter, EditorRequired]
+    public Theme Theme { get; set; } = null!;
+}
