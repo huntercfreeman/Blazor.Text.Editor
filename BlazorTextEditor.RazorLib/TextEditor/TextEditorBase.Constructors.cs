e6adff2921a0d59c9ee86008fa08e7c964d906dd
Hunter Freeman <hunter.freeman@darkmatter2bd.com>
2022-10-25 12:21:35 -0400

    TextEditorBase.Constructors.cs

---
+++ BlazorTextEditor.RazorLib/TextEditor/TextEditorBase.Constructors.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections.Immutable;
+using BlazorTextEditor.RazorLib.Decoration;
+using BlazorTextEditor.RazorLib.Keyboard;
+using BlazorTextEditor.RazorLib.Lexing;
+using BlazorTextEditor.RazorLib.Store.TextEditorCase;
+
+namespace BlazorTextEditor.RazorLib.TextEditor;
+
+public partial class TextEditorBase
+{
+    public TextEditorBase(
+        string content, 
+        ILexer? lexer, 
+        IDecorationMapper? decorationMapper)
+    {
+        Lexer = lexer ?? new LexerDefault();
+        DecorationMapper = decorationMapper ?? new DecorationMapperDefault();
+        var rowIndex = 0;
+        var previousCharacter = '\0';
+
+        var charactersOnRow = 0;
+
+        var carriageReturnCount = 0;
+        var linefeedCount = 0;
+        var carriageReturnLinefeedCount = 0;
+        
+        for (var index = 0; index < content.Length; index++)
+        {
+            var character = content[index];
+
+            charactersOnRow++;
+
+            if (character == KeyboardKeyFacts.WhitespaceCharacters.CARRIAGE_RETURN)
+            {
+                _rowEndingPositions.Add((index + 1, RowEndingKind.CarriageReturn));
+                rowIndex++;
+
+                if (charactersOnRow > MostCharactersOnASingleRow)
+                {
+                    MostCharactersOnASingleRow = charactersOnRow;
+                }
+
+                charactersOnRow = 0;
+                
+                carriageReturnCount++;
+            }
+            else if (character == KeyboardKeyFacts.WhitespaceCharacters.NEW_LINE)
+            {
+                if (previousCharacter == KeyboardKeyFacts.WhitespaceCharacters.CARRIAGE_RETURN)
+                {
+                    var lineEnding = _rowEndingPositions[rowIndex - 1];
+
+                    _rowEndingPositions[rowIndex - 1] =
+                        (lineEnding.positionIndex + 1, RowEndingKind.CarriageReturnLinefeed);
+                    
+                    carriageReturnCount--;
+                    carriageReturnLinefeedCount++;
+                }
+                else
+                {
+                    _rowEndingPositions.Add((index + 1, RowEndingKind.Linefeed));
+                    rowIndex++;
+                    
+                    if (charactersOnRow > MostCharactersOnASingleRow)
+                    {
+                        MostCharactersOnASingleRow = charactersOnRow;
+                    }
+
+                    charactersOnRow = 0;
+                    
+                    linefeedCount++;
+                }
+            }
+
+            if (character == KeyboardKeyFacts.WhitespaceCharacters.TAB)
+                _tabKeyPositions.Add(index);
+
+            previousCharacter = character;
+
+            _content.Add(new RichCharacter
+            {
+                Value = character,
+                DecorationByte = default
+            });
+        }
+
+        _rowEndingKindCounts.AddRange(new List<(RowEndingKind rowEndingKind, int count)>
+        {
+            (RowEndingKind.CarriageReturn, carriageReturnCount),
+            (RowEndingKind.Linefeed, linefeedCount),
+            (RowEndingKind.CarriageReturnLinefeed, carriageReturnLinefeedCount),
+        });
+
+        CheckRowEndingPositions(true);
+        
+        _rowEndingPositions.Add((content.Length, RowEndingKind.EndOfFile));
+    }
+
+    public TextEditorBase(
+        string content, 
+        ILexer? lexer,
+        IDecorationMapper? decorationMapper,
+        TextEditorKey key)
+            : this(content, lexer, decorationMapper)
+    {
+        Key = key;
+    }
+}
---
+++ BlazorTextEditor.RazorLib/TextEditor/TextEditorBase.Variables.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Immutable;
+using BlazorTextEditor.RazorLib.Decoration;
+using BlazorTextEditor.RazorLib.Keyboard;
+using BlazorTextEditor.RazorLib.Lexing;
+using BlazorTextEditor.RazorLib.Store.TextEditorCase;
+
+namespace BlazorTextEditor.RazorLib.TextEditor;
+
+public partial class TextEditorBase
+{
+    public const int TabWidth = 4;
+    public const int GutterPaddingLeftInPixels = 5;
+    public const int GutterPaddingRightInPixels = 15;
+    public const int MaximumEditBlocks = 10;
+    
+    /// <summary>
+    /// To get the ending position of RowIndex _rowEndingPositions[RowIndex]
+    /// <br/><br/>
+    /// _rowEndingPositions returns the start of the NEXT row
+    /// </summary>
+    private readonly List<(int positionIndex, RowEndingKind rowEndingKind)> _rowEndingPositions = new();
+
+    /// <summary>
+    /// Provides exact position index of a tab character
+    /// </summary>
+    private readonly List<int> _tabKeyPositions = new();
+
+    private readonly List<RichCharacter> _content = new();
+    private readonly List<EditBlock> _editBlocks = new();
+    private readonly List<(RowEndingKind rowEndingKind, int count)> _rowEndingKindCounts = new();
+
+    public TextEditorKey Key { get; } = TextEditorKey.NewTextEditorKey();
+    public int RowCount => _rowEndingPositions.Count;
+    public int DocumentLength => _content.Count;
+    
+    public ImmutableArray<EditBlock> EditBlocks => _editBlocks.ToImmutableArray();
+
+    /// <summary>
+    /// If there is a mixture of<br/>
+    ///     -Carriage Return<br/>
+    ///     -Linefeed<br/>
+    ///     -CRLF<br/>
+    /// Then <see cref="OnlyRowEndingKind"/> will be null.
+    /// <br/><br/>
+    /// If there are no line endings
+    /// then <see cref="OnlyRowEndingKind"/> will be null.
+    /// </summary>
+    public RowEndingKind? OnlyRowEndingKind { get; private set; }
+    public RowEndingKind UsingRowEndingKind { get; private set; }
+    public ILexer Lexer { get; private set; }
+    public IDecorationMapper DecorationMapper { get; private set; }
+    
+    public int MostCharactersOnASingleRow { get; private set; }
+
+    public TextEditorOptions TextEditorOptions { get; } = TextEditorOptions.UnsetTextEditorOptions();
+
+    public ImmutableArray<(int positionIndex, RowEndingKind rowEndingKind)> RowEndingPositions =>
+        _rowEndingPositions.ToImmutableArray();
+}
