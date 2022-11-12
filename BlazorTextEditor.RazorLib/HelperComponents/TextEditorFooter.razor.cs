777ade424bd87fb940137404b1d3b2b6905f0daa
Hunter Freeman <hunter.freeman@darkmatter2bd.com>
2022-11-12 12:09:37 -0500

    Working on ease of use for TextEditorFooter Blazor Component

--- BlazorTextEditor.RazorLib/HelperComponents/TextEditorFooter.razor.cs
+++ BlazorTextEditor.RazorLib/HelperComponents/TextEditorFooter.razor.cs
@@ -1,6 +1,8 @@
 ﻿using BlazorTextEditor.RazorLib;
 using BlazorTextEditor.RazorLib.Row;
+using BlazorTextEditor.RazorLib.Store.TextEditorCase;
 using BlazorTextEditor.RazorLib.TextEditor;
+using Fluxor;
 using Microsoft.AspNetCore.Components;
 
 namespace BlazorTextEditor.RazorLib.HelperComponents;
@@ -8,31 +10,36 @@ namespace BlazorTextEditor.RazorLib.HelperComponents;
 public partial class TextEditorFooter : ComponentBase, IDisposable
 {
     [Inject]
+    private IStateSelection<TextEditorStates, TextEditorBase?> TextEditorStatesSelection { get; set; } = null!;
+    [Inject]
+    private IState<TextEditorStates> TextEditorStatesWrap { get; set; } = null!;
+    [Inject]
     private ITextEditorService TextEditorService { get; set; } = null!;
 
     [Parameter, EditorRequired]
-    public TextEditorBase? TextEditor { get; set; }
+    public TextEditorKey TextEditorKey { get; set; } = null!;
     [Parameter, EditorRequired]
     public TextEditorDisplay? TextEditorDisplay { get; set; }
     [Parameter, EditorRequired]
     public string? FileExtension { get; set; }
 
     private TextEditorDisplay? _previousTextEditorDisplay;
+    private TextEditorOptions? _previousGlobalTextEditorOptions;
 
     protected override Task OnParametersSetAsync()
     {
         if (TextEditorDisplay is null)
         {
             if (_previousTextEditorDisplay is not null)
-                _previousTextEditorDisplay.CursorsChanged -= PreviousTextEditorDisplayOnCursorsChanged;
+                _previousTextEditorDisplay.CursorsChanged -= TextEditorDisplayOnCursorsChanged;
         }
         else if (_previousTextEditorDisplay is null ||
                  _previousTextEditorDisplay.GetHashCode() != TextEditorDisplay.GetHashCode())
         {
             if (_previousTextEditorDisplay is not null)
-                _previousTextEditorDisplay.CursorsChanged -= PreviousTextEditorDisplayOnCursorsChanged;
+                _previousTextEditorDisplay.CursorsChanged -= TextEditorDisplayOnCursorsChanged;
 
-            TextEditorDisplay.CursorsChanged += PreviousTextEditorDisplayOnCursorsChanged;
+            TextEditorDisplay.CursorsChanged += TextEditorDisplayOnCursorsChanged;
         }
 
         _previousTextEditorDisplay = TextEditorDisplay;
@@ -40,17 +47,35 @@ public partial class TextEditorFooter : ComponentBase, IDisposable
         return base.OnParametersSetAsync();
     }
 
-    private void PreviousTextEditorDisplayOnCursorsChanged()
+    protected override void OnInitialized()
+    {
+        TextEditorStatesWrap.StateChanged += TextEditorStatesWrapOnStateChanged;
+
+        TextEditorStatesSelection
+            .Select(textEditorStates => textEditorStates.TextEditorList
+                .SingleOrDefault(x => x.Key == TextEditorKey));
+        
+        base.OnInitialized();
+    }
+    
+    private void TextEditorStatesWrapOnStateChanged(object? sender, EventArgs e)
+    {
+        if (_previousGlobalTextEditorOptions is null ||
+            _previousGlobalTextEditorOptions != TextEditorStatesWrap.Value.GlobalTextEditorOptions)
+        {
+            _previousGlobalTextEditorOptions = TextEditorStatesWrap.Value.GlobalTextEditorOptions;
+            InvokeAsync(StateHasChanged);
+        }
+    }
+
+    private void TextEditorDisplayOnCursorsChanged()
     {
         InvokeAsync(StateHasChanged);
     }
 
     private void SelectRowEndingKindOnChange(ChangeEventArgs changeEventArgs)
     {
-        var textEditorKey = TextEditor?.Key;
-
-        if (textEditorKey is null)
-            return;
+        var textEditorKey = TextEditorKey;
 
         var rowEndingKindString = (string)(changeEventArgs.Value ?? string.Empty);
 
@@ -61,6 +86,8 @@ public partial class TextEditorFooter : ComponentBase, IDisposable
     public void Dispose()
     {
         if (TextEditorDisplay is not null)
-            TextEditorDisplay.CursorsChanged -= PreviousTextEditorDisplayOnCursorsChanged;
+            TextEditorDisplay.CursorsChanged -= TextEditorDisplayOnCursorsChanged;
+        
+        TextEditorStatesWrap.StateChanged -= TextEditorStatesWrapOnStateChanged;
     }
 }
