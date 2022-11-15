5a45ff6d8d164ad78de6d50b1572e575d8644485
Hunter Freeman <hunter.freeman@darkmatter2bd.com>
2022-11-14 21:56:58 -0500

    Consistent theme in the optional static dialog for changing settings

--- BlazorTextEditor.RazorLib/Dialog/DialogDisplay.razor.cs
+++ BlazorTextEditor.RazorLib/Dialog/DialogDisplay.razor.cs
@@ -1,14 +1,21 @@
 using BlazorTextEditor.RazorLib.Resize;
 using BlazorTextEditor.RazorLib.Store.DialogCase;
+using BlazorTextEditor.RazorLib.Store.TextEditorCase;
+using BlazorTextEditor.RazorLib.TextEditor;
 using Fluxor;
+using Fluxor.Blazor.Web.Components;
 using Microsoft.AspNetCore.Components;
 
 namespace BlazorTextEditor.RazorLib.Dialog;
 
-public partial class DialogDisplay : ComponentBase
+public partial class DialogDisplay : FluxorComponent
 {
     [Inject]
     private IDispatcher Dispatcher { get; set; } = null!;
+    [Inject]
+    private ITextEditorService TextEditorService { get; set; } = null!;
+    [Inject]
+    private IState<TextEditorStates> TextEditorStatesWrap { get; set; } = null!;
 
     [Parameter]
     public DialogRecord DialogRecord { get; set; } = null!;
@@ -16,6 +23,13 @@ public partial class DialogDisplay : ComponentBase
     private ResizableDisplay? _resizableDisplay;
 
     private string StyleCssString => DialogRecord.ElementDimensions.StyleString;
+    
+    private string GlobalThemeCssClassString => TextEditorService
+                                                    .TextEditorStates
+                                                    .GlobalTextEditorOptions
+                                                    .Theme?
+                                                    .CssClassString
+                                                ?? string.Empty;
     
     private async Task ReRenderAsync()
     {
