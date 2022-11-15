017c8d47d81121bdd4fa92f46d29704ed15a337c
Hunter Freeman <hunter.freeman@darkmatter2bd.com>
2022-11-15 00:53:50 -0500

    Virtualization move to separate files the many classes in one

---
+++ BlazorTextEditor.RazorLib/Virtualization/VirtualizationBoundary.cs
@@ -0,0 +1,7 @@
+﻿namespace BlazorTextEditor.RazorLib.Virtualization;
+
+public record VirtualizationBoundary(
+    double? WidthInPixels,
+    double? HeightInPixels,
+    double? LeftInPixels,
+    double? TopInPixels);
---
+++ BlazorTextEditor.RazorLib/Virtualization/VirtualizationResult.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Immutable;
+
+namespace BlazorTextEditor.RazorLib.Virtualization;
+
+public record VirtualizationResult<T>(
+    ImmutableArray<VirtualizationEntry<T>> Entries,
+    VirtualizationBoundary LeftVirtualizationBoundary,
+    VirtualizationBoundary RightVirtualizationBoundary,
+    VirtualizationBoundary TopVirtualizationBoundary,
+    VirtualizationBoundary BottomVirtualizationBoundary);
