fc391a02095ed64973af26afdbf5951102f54391
Hunter Freeman <hunter.freeman@darkmatter2bd.com>
2022-11-18 14:29:52 -0500

    Film documentation

---
+++ BlazorTextEditor.Demo.Wasm/Shared/MainLayout.razor.cs
@@ -0,0 +1,22 @@
+﻿using BlazorTextEditor.RazorLib;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorTextEditor.Demo.Wasm.Shared;
+
+public partial class MainLayout : LayoutComponentBase
+{
+    [Inject]
+    private ITextEditorService TextEditorService { get; set; } = null!;
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender)
+        {
+            await TextEditorService.SetTextEditorOptionsFromLocalStorageAsync();
+
+            await InvokeAsync(StateHasChanged);
+        }
+        
+        await base.OnAfterRenderAsync(firstRender);
+    }
+}
