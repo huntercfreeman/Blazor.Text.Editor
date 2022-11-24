25fd163bf4b58f6e7d01c960902d3958c9098762
Hunter Freeman <hunter.freeman@darkmatter2bd.com>
2022-11-24 15:22:12 -0500

    Scroll by dragging mouse on horizontal scrollbar

--- BlazorTextEditor.RazorLib/Scrollbar/ScrollbarHorizontal.razor.cs
+++ BlazorTextEditor.RazorLib/Scrollbar/ScrollbarHorizontal.razor.cs
@@ -3,11 +3,16 @@ using BlazorTextEditor.RazorLib.JavaScriptObjects;
 using BlazorTextEditor.RazorLib.TextEditor;
 using BlazorTextEditor.RazorLib.Virtualization;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.JSInterop;
 
 namespace BlazorTextEditor.RazorLib.Scrollbar;
 
 public partial class ScrollbarHorizontal : ComponentBase
 {
+    [Inject]
+    private IJSRuntime JsRuntime { get; set; } = null!;
+    
     [CascadingParameter]
     public TextEditorBase TextEditor { get; set; } = null!;
     [CascadingParameter]
@@ -19,6 +24,15 @@ public partial class ScrollbarHorizontal : ComponentBase
     public WidthAndHeightOfTextEditor WidthAndHeightOfTextEditor { get; set; } = null!;
     [Parameter, EditorRequired]
     public TextEditorDisplay TextEditorDisplay { get; set; } = null!;
+    
+    private readonly SemaphoreSlim _onMouseMoveSemaphoreSlim = new(1, 1);
+    private readonly TimeSpan _onMouseMoveDelay = TimeSpan.FromMilliseconds(25);
+    
+    private bool _thinksLeftMouseButtonIsDown;
+
+    private readonly Guid _scrollbarGuid = Guid.NewGuid();
+    
+    private string ScrollbarElementId => $"bte_{_scrollbarGuid}";
     
     private string GetScrollbarHorizontalStyleCss()
     {
@@ -67,5 +81,56 @@ public partial class ScrollbarHorizontal : ComponentBase
         var width = $"width: {sliderProportionalWidthInPixels}px;";
         
         return $"{left} {width}";
+    }
+    
+    private Task HandleOnMouseDownAsync(MouseEventArgs arg)
+    {
+        _thinksLeftMouseButtonIsDown = true;
+
+        return Task.CompletedTask;
+    }
+    
+    private async Task HandleOnMouseMoveAsync(MouseEventArgs mouseEventArgs)
+    {
+        var success = await _onMouseMoveSemaphoreSlim
+            .WaitAsync(TimeSpan.Zero);
+
+        if (!success)
+            return;
+
+        try
+        {
+            // Buttons is a bit flag
+            // '& 1' gets if left mouse button is held
+            if (_thinksLeftMouseButtonIsDown &&
+                (mouseEventArgs.Buttons & 1) == 1)
+            {
+                var relativeCoordinatesOnClick = await JsRuntime
+                    .InvokeAsync<RelativeCoordinates>(
+                        "blazorTextEditor.getRelativePosition",
+                        ScrollbarElementId,
+                        mouseEventArgs.ClientX,
+                        mouseEventArgs.ClientY);
+                
+                var scrollbarWidthInPixels = WidthAndHeightOfTextEditor.WidthInPixels - 
+                                              ScrollbarFacts.SCROLLBAR_SIZE_IN_PIXELS;
+
+                var scrollLeft = relativeCoordinatesOnClick.RelativeX *
+                                VirtualizationResult.VirtualizationScrollPosition.ScrollWidthInPixels /
+                                scrollbarWidthInPixels;
+
+                await TextEditorDisplay.SetScrollPositionAsync(scrollLeft, null);
+            }
+            else
+            {
+                _thinksLeftMouseButtonIsDown = false;
+            }
+
+            await Task.Delay(_onMouseMoveDelay);
+        }
+        finally
+        {
+            _onMouseMoveSemaphoreSlim.Release();
+        }
     }
 }
