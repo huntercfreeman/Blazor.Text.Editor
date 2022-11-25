e7232dcc0be7f085393d49b31d01068fc0588f8e
Hunter Freeman <hunter.freeman@darkmatter2bd.com>
2022-11-25 01:09:07 -0500

    Use C# example in demo

--- BlazorTextEditor.Demo.Wasm/Pages/Index.razor.cs
+++ BlazorTextEditor.Demo.Wasm/Pages/Index.razor.cs
@@ -14,39 +14,7 @@ public partial class Index : ComponentBase
     {
         TextEditorService.RegisterCSharpTextEditor(
             TextEditorFacts.CSharpTextEditor,
-            string.Empty);
-        
-        // TextEditorService.RegisterCSharpTextEditor(
-        //     TextEditorFacts.CSharpTextEditor,
-        //     TestData.CSharp.EXAMPLE_TEXT_173_LINES);
-        
-        TextEditorService.RegisterCssTextEditor(
-            TextEditorFacts.CssTextEditor,
-            TestData.Css.EXAMPLE_TEXT_21_LINES);
-        
-        TextEditorService.RegisterFSharpTextEditor(
-            TextEditorFacts.FSharpTextEditor,
-            TestData.FSharp.EXAMPLE_TEXT_21_LINES);
-        
-        TextEditorService.RegisterHtmlTextEditor(
-            TextEditorFacts.HtmlTextEditor,
-            TestData.Html.EXAMPLE_TEXT_19_LINES);
-        
-        TextEditorService.RegisterJavaScriptTextEditor(
-            TextEditorFacts.JavaScriptTextEditor,
-            TestData.JavaScript.EXAMPLE_TEXT_28_LINES);
-        
-        TextEditorService.RegisterPlainTextEditor(
-            TextEditorFacts.PlainTextEditor,
-            TestData.Plain.EXAMPLE_TEXT_25_LINES);
-        
-        TextEditorService.RegisterRazorTextEditor(
-            TextEditorFacts.RazorTextEditor,
-            TestData.Razor.EXAMPLE_TEXT_20_LINES);
-        
-        TextEditorService.RegisterTypeScriptTextEditor(
-            TextEditorFacts.TypeScriptTextEditor,
-            TestData.TypeScript.EXAMPLE_TEXT_28_LINES);
+            TestData.CSharp.EXAMPLE_TEXT_173_LINES);
         
         base.OnInitialized();
     }
