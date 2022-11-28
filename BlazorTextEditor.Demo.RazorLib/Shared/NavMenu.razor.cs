ab65a6797ae1b4cac838bf213831c5b7acf32888
Hunter Freeman <hunter.freeman@darkmatter2bd.com>
2022-11-28 13:42:41 -0500

    Show button to open text editor settings in the NavMenu.razor

---
+++ BlazorTextEditor.Demo.RazorLib/Shared/NavMenu.razor.cs
@@ -0,0 +1,24 @@
+﻿using BlazorTextEditor.RazorLib;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorTextEditor.Demo.RazorLib.Shared;
+
+public partial class NavMenu : ComponentBase
+{
+    [Inject]
+    private ITextEditorService TextEditorService { get; set; } = null!;
+    
+    private bool _collapseNavMenu = true;
+
+    private string? NavMenuCssClass => _collapseNavMenu ? "collapse" : null;
+
+    private void ToggleNavMenu()
+    {
+        _collapseNavMenu = !_collapseNavMenu;
+    }
+    
+    private void OpenTextEditorSettingsDialogOnClick()
+    {
+        TextEditorService.ShowSettingsDialog(true);
+    }
+}
