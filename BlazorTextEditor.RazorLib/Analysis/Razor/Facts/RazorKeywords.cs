4f55207c6011ac7cd133a1d1dd1a8c672b627f3e
Hunter Freeman <hunter.freeman@darkmatter2bd.com>
2022-12-06 16:06:07 -0500

    Razor keywords

---
+++ BlazorTextEditor.RazorLib/Analysis/Razor/Facts/RazorKeywords.cs
@@ -0,0 +1,25 @@
+using System.Collections.Immutable;
+
+namespace BlazorTextEditor.RazorLib.Analysis.Razor.Facts;
+
+public static class RazorKeywords
+{
+    public const string PAGE_KEYWORD = "page";
+    public const string NAMESPACE_KEYWORD = "namespace";
+    public const string FUNCTIONS_KEYWORD = "functions";
+    public const string INHERITS_KEYWORD = "inherits";
+    public const string MODEL_KEYWORD = "model";
+    public const string SECTION_KEYWORD = "section";
+    public const string HELPER_KEYWORD = "helper";
+    
+    public static readonly ImmutableArray<string> ALL = new[]
+    {
+        PAGE_KEYWORD,
+        NAMESPACE_KEYWORD,
+        FUNCTIONS_KEYWORD,
+        INHERITS_KEYWORD,
+        MODEL_KEYWORD,
+        SECTION_KEYWORD,
+        HELPER_KEYWORD,
+    }.ToImmutableArray();
+}
