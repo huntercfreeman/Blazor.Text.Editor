3bb07e8a815d579c11b1e35caf5211fe696ae3d9
Hunter Freeman <hunter.freeman@darkmatter2bd.com>
2022-12-29 21:10:32 -0500

    Vim Progress on delete verb

--- BlazorTextEditor.RazorLib/Keymap/VimKeymapSpecifics/VimVerbFacts.cs
+++ BlazorTextEditor.RazorLib/Keymap/VimKeymapSpecifics/VimVerbFacts.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.Immutable;
 using BlazorTextEditor.RazorLib.Commands;
+using BlazorTextEditor.RazorLib.Cursor;
+using BlazorTextEditor.RazorLib.Store.TextEditorCase.Actions;
 using Microsoft.AspNetCore.Components.Web;
 
 namespace BlazorTextEditor.RazorLib.Keymap.VimKeymapSpecifics;
@@ -34,7 +36,108 @@ public static class VimVerbFacts
         bool hasTextSelection,
         out TextEditorCommand textEditorCommand)
     {
-        textEditorCommand = TextEditorCommandFacts.DoNothingDiscard;
-        return true;
+        bool verbHasRepeat = false;
+        
+        var currentToken = sentenceSnapshot[indexInSentence];
+
+        if (indexInSentence + 1 < sentenceSnapshot.Length)
+        {
+            var nextToken = sentenceSnapshot[indexInSentence + 1];
+
+            if (nextToken.VimGrammarKind == VimGrammarKind.Verb &&
+                nextToken.TextValue == currentToken.TextValue)
+            {
+                verbHasRepeat = true;
+            }
+        }
+
+        var success = false;
+
+        if (verbHasRepeat)
+        {
+            // TODO: When a verb is repeated is it always the case that the position indices to operate over are known without the need of a motion? Example, "dd" would delete the current line and copy it to the in memory clipboard. But no motion was needed to know what text to delete.
+
+            success = true;
+            
+            switch (currentToken.TextValue)
+            {
+                case "d":
+                {
+                    textEditorCommand = new TextEditorCommand(
+                        async textEditorCommandParameter =>
+                        {                         
+                            await TextEditorCommandFacts.Cut.DoAsyncFunc
+                                .Invoke(textEditorCommandParameter);
+                        },
+                        true,
+                        "Vim::Delete(Line)",
+                        "Vim::Delete(Line)");
+                    
+                    break;
+                }
+                default:
+                {
+                    textEditorCommand = TextEditorCommandFacts.DoNothingDiscard;
+                    break;
+                }
+            }
+        }
+        else
+        {
+            success = VimSentence.TryParseMoveNext(
+                sentenceSnapshot,
+                indexInSentence + 1,
+                keyboardEventArgs,
+                hasTextSelection,
+                out var innerTextEditorCommand);
+
+            var displayName = 
+                $"Vim::Delete({innerTextEditorCommand.DisplayName})";
+
+            textEditorCommand = new TextEditorCommand(
+                async textEditorCommandParameter =>
+                {
+                    var currentPrimaryImmutableCursor = textEditorCommandParameter
+                        .PrimaryCursorSnapshot.ImmutableCursor;
+                    
+                    await innerTextEditorCommand.DoAsyncFunc
+                        .Invoke(textEditorCommandParameter);
+
+                    var motionedPrimaryImmutableCursor = new ImmutableTextEditorCursor(
+                        textEditorCommandParameter.PrimaryCursorSnapshot.UserCursor);
+
+                    var startingPositionIndexInclusive = textEditorCommandParameter.TextEditorBase.GetPositionIndex(
+                        currentPrimaryImmutableCursor.RowIndex,
+                        currentPrimaryImmutableCursor.ColumnIndex);
+                    
+                    var endingPositionIndexExclusive = textEditorCommandParameter.TextEditorBase.GetPositionIndex(
+                        motionedPrimaryImmutableCursor.RowIndex,
+                        motionedPrimaryImmutableCursor.ColumnIndex);
+
+                    var removeCharacterCount = endingPositionIndexExclusive - startingPositionIndexInclusive;
+                    
+                    var cursorForDeletion = new TextEditorCursor(
+                        (currentPrimaryImmutableCursor.RowIndex, 
+                            currentPrimaryImmutableCursor.ColumnIndex),
+                        true);
+                    
+                    var deleteTextTextEditorBaseAction = new DeleteTextByRangeTextEditorBaseAction(
+                        textEditorCommandParameter.TextEditorBase.Key,
+                        TextEditorCursorSnapshot.TakeSnapshots(cursorForDeletion),
+                        removeCharacterCount,
+                        CancellationToken.None);
+                
+                    textEditorCommandParameter
+                        .TextEditorService
+                        .DeleteTextByRange(deleteTextTextEditorBaseAction); 
+                },
+                true,
+                displayName,
+                displayName);
+            
+            
+        }
+        
+        return success;
     }
 }
