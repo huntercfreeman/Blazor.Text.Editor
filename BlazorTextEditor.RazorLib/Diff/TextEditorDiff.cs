f55678a2191377556e24240aef15fc92e6f04378
Hunter Freeman <hunter.freeman@darkmatter2bd.com>
2023-01-26 19:01:23 -0500

    Making some classes for TextEditorDiff.cs

---
+++ BlazorTextEditor.RazorLib/Diff/TextEditorDiff.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Immutable;
+using BlazorTextEditor.RazorLib.Store.TextEditorCase.Misc;
+using BlazorTextEditor.RazorLib.ViewModel;
+
+namespace BlazorTextEditor.RazorLib.Diff;
+
+public record TextEditorDiff(
+    TextEditorDiffKey TextEditorDiffKey,
+    TextEditorViewModelKey BeforeViewModelKey,
+    TextEditorViewModelKey AfterViewModelKey,
+    ImmutableList<TextEditorViewModelKey> ViewModelKeys)
+{
+    public TextEditorRenderStateKey TextEditorRenderStateKey { get; init; } = TextEditorRenderStateKey.NewTextEditorRenderStateKey();
+}
