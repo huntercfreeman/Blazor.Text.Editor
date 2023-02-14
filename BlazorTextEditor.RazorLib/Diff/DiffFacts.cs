e88f2bcd4b1eb3283fde9c377d5f0d149a7887b6
Hunter Freeman <hunter.freeman@darkmatter2bd.com>
2023-02-14 14:19:05 -0500

    Working on rendering diff presentation

---
+++ BlazorTextEditor.RazorLib/Diff/DiffFacts.cs
@@ -0,0 +1,9 @@
+﻿using BlazorTextEditor.RazorLib.Decoration;
+
+namespace BlazorTextEditor.RazorLib.Diff;
+
+public static class DiffFacts
+{
+    public static readonly TextEditorPresentationKey PresentationKey = TextEditorPresentationKey.NewTextEditorPresentationKey();
+    public const string CssClassString = "bte_diff-presentation";
+}
