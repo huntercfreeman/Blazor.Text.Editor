52b0e72ed5b0ca0021257d2ede73e34d3b4399cd
Hunter Freeman <huntercfreeman@gmail.com>
2023-03-15 04:44:29 -0400

    Trying to render insertion presentation (green background)

---
+++ BlazorTextEditor.Tests/Diff/2023-03-15/DiffContiguousRepetitionOfCharactersTests.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorTextEditor.Tests.Diff._2023_03_15;
+
+public class DiffContiguousRepetitionOfCharactersTests
+{
+    // TODO: Add test cases for:
+    //     beforeText = "aa"
+    //     afterText = "aaaa"
+    //
+    // I cannot recall exactly what the issue was, but while writing
+    // the algorithm initially this had a bug.
+    //
+    // Going to work on other tasks before this.
+}
---
+++ BlazorTextEditor.Tests/Diff/2023-03-15/DiffSingleCharacterTests.cs
@@ -0,0 +1,48 @@
+﻿using BlazorTextEditor.RazorLib.Diff;
+
+namespace BlazorTextEditor.Tests.Diff._2023_03_15;
+
+public class DiffSingleCharacterTests
+{
+    [Fact]
+    public void InputsAreNotEqual()
+    {
+        // Input
+        var beforeText = "a";
+        var afterText = "b";
+        
+        // Expected
+        var expectedLongestCommonSubsequence = string.Empty;
+
+        // Calculate
+        var diffResult = TextEditorDiffResult.Calculate(
+            beforeText,
+            afterText);
+
+        // Assert
+        Assert.Equal(
+            expectedLongestCommonSubsequence,
+            diffResult.LongestCommonSubsequence);
+    }
+    
+    [Fact]
+    public void InputsAreEqual()
+    {
+        // Input
+        var beforeText = "a";
+        var afterText = "a";
+        
+        // Expected
+        var expectedLongestCommonSubsequence = "a";
+
+        // Calculate
+        var diffResult = TextEditorDiffResult.Calculate(
+            beforeText,
+            afterText);
+
+        // Assert
+        Assert.Equal(
+            expectedLongestCommonSubsequence,
+            diffResult.LongestCommonSubsequence);
+    }
+}
