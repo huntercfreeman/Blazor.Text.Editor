7b60c22f3938eb78f36a884636eb2e13a8039be1
Hunter Freeman <huntercfreeman@gmail.com>
2023-04-07 13:46:41 -0400

    Render Referenced NuGet Packages in TreeView

--- BlazorTextEditor.RazorLib/Analysis/Html/SyntaxActors/HtmlSyntaxWalker.cs
+++ BlazorTextEditor.RazorLib/Analysis/Html/SyntaxActors/HtmlSyntaxWalker.cs
@@ -1,5 +1,4 @@
-﻿using BlazorTextEditor.RazorLib.Analysis.Html.SyntaxEnums;
-using BlazorTextEditor.RazorLib.Analysis.Html.SyntaxObjects;
+﻿using BlazorTextEditor.RazorLib.Analysis.Html.SyntaxObjects;
 
 namespace BlazorTextEditor.RazorLib.Analysis.Html.SyntaxActors;
 
