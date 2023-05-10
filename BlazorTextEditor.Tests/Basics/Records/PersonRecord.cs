43dac90cb68bdf00164b45c0c33485ca7e079d5a
Hunter Freeman <huntercfreeman@gmail.com>
2023-05-10 18:35:56 -0400

    Made a unit test for sanity check

---
+++ BlazorTextEditor.Tests/Basics/Records/PersonRecord.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorTextEditor.Tests.Basics.Records
+{
+    internal record PersonRecord
+    {
+        public PersonRecord(
+            Guid id,
+            string description,
+            string firstName,
+            string lastName)
+        {
+            Id = id;
+            Description = description;
+            FirstName = firstName;
+            LastName = lastName;
+        }                   
+
+        public Guid Id { get; init; }
+        public string Description { get; init; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
